9557d5e6bfd6fe7cee5b6852a91965d180d2eebd
jeevakrishan96 <jeevakrishan96@gmail.com>
2023-05-30 11:52:28 +0530

    test11

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
-    cy.console.log("hai");
-    cy.console.log("hai");
+    cy.log("hai");
+    cy.log("hai");
   })
 })
